7c56ca69183c848f620dcd42a8dde44415055561
randiandrio <randiandrio@gmail.com>
2024-11-27 16:31:16 +0700

    tps id

--- app/real-count/page.tsx
+++ app/real-count/page.tsx
@@ -218,7 +218,7 @@ function RealCount() {
     {
       name: "Tanggal Data",
       width: "200px",
-      selector: (row) => tglJamIndo(row.updatedAt),
+      selector: (row) => `${tglJamIndo(row.updatedAt)} - ${row.tps.id}`,
       sortable: true,
     },
     {

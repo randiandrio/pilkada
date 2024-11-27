136b87965aca6a107407f5e40fad63351a9eefce
randiandrio <randiandrio@gmail.com>
2024-11-27 16:08:45 +0700

    abaikan error

--- app/real-count/page.tsx
+++ app/real-count/page.tsx
@@ -167,7 +167,9 @@ function RealCount() {
         // Kolom dinamis
         const dynamicCols = x.paslon.map((item: Paslon, index: number) => ({
           name: `${item.calon} / ${item.wakil}`,
-          selector: (row: any) => String(row.detail[index].suara ?? 0),
+          selector: (row: any) => {
+            row.detail != undefined ? String(row.detail[index].suara) : "0";
+          },
           cell: (row: any) => (
             <div
               style={{
@@ -181,7 +183,7 @@ function RealCount() {
                 alignItems: "center",
               }}
             >
-              {String(row.detail[index].suara ?? 0)}
+              {row.detail != undefined ? String(row.detail[index].suara) : "0"}
             </div>
           ),
         }));
